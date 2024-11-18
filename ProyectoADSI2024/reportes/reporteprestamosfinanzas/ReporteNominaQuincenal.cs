638891782cf1275182923d2a92c44b9cf7ce5b2c
EugeneWu11 <genewu013@gmail.com>
2024-11-17 20:23:17 -0600

    ReporteNominaQuincenal en proceso

--- ProyectoADSI2024/reportes/reporteprestamosfinanzas/ReporteNominaQuincenal.cs
+++ ProyectoADSI2024/reportes/reporteprestamosfinanzas/ReporteNominaQuincenal.cs
@@ -87,6 +87,14 @@ namespace ProyectoADSI2024.Reportes.ReportePrestamosFinanzas {
                 return this.ReportDefinition.Sections[4];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_Nombre {
+            get {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
