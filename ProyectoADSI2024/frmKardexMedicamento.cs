7b60d4010cd55dcad4618d45f6b008f441b36f60
Evelyn Sabillon <evelyn.sabillon@unah.hn>
2024-11-22 13:37:03 -0600

    Cambios en formularios de Kardex para adaptar el rpt

--- ProyectoADSI2024/frmKardexMedicamento.cs
+++ ProyectoADSI2024/frmKardexMedicamento.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,35 @@ namespace ProyectoADSI2024
 {
     public partial class frmKardexMedicamento : Form
     {
+        private Conexion conexion; //Conexion con SQL Server
+        SqlDataAdapter adaptador; //Adaptador para SQL Server
+        DataTable tabla; //Tabla para SQL Server
         public frmKardexMedicamento()
         {
             InitializeComponent();
+            conexion = new Conexion(); //Inicializar la conexion
+            adaptador = new SqlDataAdapter("spArticuloMedicamentoSelect", conexion.ObtenerConexion()); //Inicializar el adaptador
+            adaptador.SelectCommand.CommandType = CommandType.StoredProcedure; //Tipo de comando
+            tabla = new DataTable(); //Inicializar la tabla
         }
 
         private void btnAtras_Click(object sender, EventArgs e)
         {
             if (MessageBox.Show("¿Desea volver al menu principal?", "Confirmacion", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                 this.Dispose();
+        }
+
+        private void frmKardexMedicamento_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                adaptador.Fill(tabla); //Llenar la tabla
+                dataGridView1.DataSource = tabla; //Mostrar la tabla en el DataGridView
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los datos: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
