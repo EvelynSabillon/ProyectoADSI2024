e6cba93843f6fdf1df9255cc649ee8d4d301aa67
karlos1000000 <carlos.osegueda@unah.hn>
2024-11-25 23:31:19 -0600

    Mejora de formatos de Reporte Produccion, Boton nuevo de Produccion hecho

--- ProyectoADSI2024/Menu.cs
+++ ProyectoADSI2024/Menu.cs
@@ -473,7 +473,17 @@ namespace ProyectoADSI2024
 
         private void reporteDeProducciónGeneralToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            frmReporteProduccionGeneral frmReporteProduccionGral = new frmReporteProduccionGeneral();
 
+            // Obtener el número de reporte generado desde el procedimiento almacenado
+            Report_Manager reportManager = new Report_Manager();
+            string tipoReporte = "06"; // Tipo de reporte (puedes adaptarlo según sea necesario)
+            string numeroReporte = reportManager.GenerateReportNumber(tipoReporte);
+
+            // Pasar el número de reporte al formulario
+            frmReporteProduccionGral.NumeroReporte = numeroReporte;
+
+            frmReporteProduccionGral.ShowDialog();
         }
 
         private void reporteDeConsumoInsumoToolStripMenuItem_Click(object sender, EventArgs e)
